a2c8dfc4007fe0b2dd3b5ac420deebe8e10804a2
pranavkumar-373 <pranav.kumar@iiitb.org>
2022-05-01 21:59:50 +0530

    starting front end dev app.js set

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import {Provider} from 'react-redux';
+
+import './App.css';
+
+
 function App() {
-  return (
-    <div>
-      <h1>
-          Aight
-      </h1>
-    </div>
+  return ( 
+    <BrowserRouter>
+      <div className="App">
+      </div>
+    </BrowserRouter>
   );
 }
 
