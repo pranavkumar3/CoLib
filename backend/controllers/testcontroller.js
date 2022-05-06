579c01b4b222c3bf68d49122e2884c1c797121fb
Ch Akash <akash.chunduri@iiitb.org>
2022-05-06 18:50:39 +0530

    tested mogodb CRUD

--- backend/controllers/testcontroller.js
+++ backend/controllers/testcontroller.js
@@ -1,16 +1,45 @@
-const testget=(req,res)=>{
-    res.status(200).json({message:"TEST3"})
-}
+const asyncHandler = require('express-async-handler')
+const Goal=require('../models/goalModel')
 
-const testpost=(req,res)=>{
-    res.status(200).json({message:"TEST2 post"})
+const testget=asyncHandler(async (req,res)=>{
+    const ggoals= await Goal.find()
+    res.status(200).json(ggoals)
 }
+)
 
-const testput=(req,res)=>{
-    res.status(200).json({message:`TEST2 ${req.params.id}`})
+const testpost=asyncHandler( async (req,res)=>{
+    console.log("AAAAA")
+    console.log(req.body)
+    if(!req.body.text){
+        res.status(400)
+        throw new Error('Please add text')
+    }
+    const goal=await Goal.create({
+        text: req.body.text
+    })
+    res.status(200).json(goal)
 }
+)
 
-const testdelete=(req,res)=>{
-    res.status(200).json({message:`TEST2 ${req.params.id}`})
+const testput=asyncHandler(async (req,res)=>{
+    const goal =await Goal.findById(req.params.id)
+    if(!goal){
+        res.status(400)
+        throw new Error("Goal not found")
+    }
+    const updatedGoal=await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true,})
+    res.status(200).json(updatedGoal)
 }
+)
+
+const testdelete=asyncHandler(async(req,res)=>{
+    const goal =await Goal.findById(req.params.id)
+    if(!goal){
+        res.status(400)
+        throw new Error("Goal not found")
+    }
+    await goal.remove()
+    res.status(200).json({id:req.params.id})
+}
+)
 module.exports={testget,testpost,testput,testdelete}
