8cbff04fcc58fc403cdb3a7c7339d79d3d2118b2
Ch Akash <akash.chunduri@iiitb.org>
2022-05-06 19:04:55 +0530

    Added schemas for the databsae

---
+++ backend/models/issueModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const issueSchema = new Schema(
+    {
+    student: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    book: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Book',
+        required: true
+    },
+    returned: {
+        type: Boolean,
+        default: false
+    }
+}, 
+    {
+    timestamps: true
+    }
+);
+
+module.exports=mongoose.model('Issue',issueSchema);;
---
+++ backend/models/userModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const plm = require('passport-local-mongoose');
+
+const userSchema = new Schema(
+    {
+    firstname: {
+        type: String,
+          required: true,      
+    },
+    lastname: {
+        type: String,
+         required: true,     
+    },
+    email: {
+          type: String,
+          required: true,
+          unique: true
+    },
+    roll: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    admin: {
+        type: Boolean,
+        default: false
+    }
+}
+);
+
+User.plugin(plm);
+module.exports = mongoose.model('User', userSchema);
