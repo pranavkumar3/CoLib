2201795af38a46b69a6aee7d8dda8eff58e61811
Akash-Ch <43777641+Akash-Ch@users.noreply.github.com>
2022-05-12 03:29:41 +0530

    add manually to hosts

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
+#echo "Ansible Entrypoint"
+
+# echo "[ubuntu18]" >> /hosts
+# echo "172.16.130.188" >> /hosts
+
+# echo "[all:vars]" >> /hosts
+# echo "ansible_connection=ssh" >> /hosts
+# echo "ansible_user= $SSH_USER" >> /hosts
+# echo "ansible_ssh_user= $SSH_USER" >> /hosts
+# echo "ansible_python_interpreter=/usr/bin/python3.6" >> /hosts
+# echo "$SSH_USER"
+
+# echo "ansible_ssh_pass=$SSH_PASSWORD" >> /hosts
+# echo "ansible_become_pass=$SSH_PASSWORD" >> /hosts
 echo "Ansible Entrypoint"
 
-echo "[ubuntu18]" >> /hosts
-echo "172.16.130.188" >> /hosts
-
-echo "[all:vars]" >> /hosts
-echo "ansible_connection=ssh" >> /hosts
-echo "ansible_user= $SSH_USER" >> /hosts
-echo "ansible_ssh_user= $SSH_USER" >> /hosts
-echo "ansible_python_interpreter=/usr/bin/python3.6" >> /hosts
-echo "$SSH_USER"
-
-echo "ansible_ssh_pass=$SSH_PASSWORD" >> /hosts
-echo "ansible_become_pass=$SSH_PASSWORD" >> /hosts
+echo "This is the secret: $SSH_PASSWORD"
 
 echo "Entering the ansible using ansible-playbook"
 
