f6b7aacdb34292ff4a1110f7f02a3755eb89b314
Ch Akash <akash.chunduri@iiitb.org>
2022-05-12 12:47:29 +0530

    added logger

---
+++ backend/logger.js
@@ -0,0 +1,51 @@
+const winston = require('winston');
+const env = process.env.NODE_ENV;
+const logDir = 'logs';
+const fs = require('fs');
+
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+ }
+
+const now = new Date();
+var logger = winston.createLogger({
+        format: combine(
+      timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+      errors({ stack: true }),
+      logFormat
+    ),
+transports: [
+
+    new winston.transports.File({
+        name: 'error-logs',
+        filename: './logs/exceptions.log',
+        level: 'error',
+        json: false
+    }),
+
+    new winston.transports.File({
+        name: 'info-logs',
+        filename: './logs/info.log',
+        level: 'info',
+        json: false
+    }),
+
+    new(require('winston-daily-rotate-file'))({
+        filename: `${logDir}/-apimodules.log`,
+        timestamp: now,
+        datePattern: 'dd-MM-yyyy',
+        prepend: true,
+        json: false,
+        level: env === 'development' ? 'verbose' : 'info'
+    })
+],
+exitOnError: false
+});
+
+module.exports = logger;
+module.exports.stream = {
+  write: function(message, encoding) {
+    logger.info(message);
+    console.log('message=', message);
+  }
+};
